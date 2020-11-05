0de0848b7c36c88ef1afd4624566cfa48dcfd612
Diane <dchampda@student.42.fr>
2020-11-05 13:40:19 +0100

    pwd added

--- srcs_minishell/ft_pwd.c
+++ srcs_minishell/ft_pwd.c
@@ -2,6 +2,10 @@
 
 int		ft_pwd()
 {
+	char	str[255];
 
+	//Inclure une fonction pour gérer automatiquement la longueur de la string ?
+	//Avec du malloc, et on rajoute de la memoire si la string est trop grande ?
+	ft_putstr(getcwd(str, 256));
 	return (1);
 }
