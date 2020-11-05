8f446803338504078bb86cf9913cd446d5c94186
Diane <dchampda@student.42.fr>
2020-11-05 21:59:17 +0100

    Recup adresse root

--- srcs_minishell/ft_cd.c
+++ srcs_minishell/ft_cd.c
@@ -1,9 +1,21 @@
 #include "../header/minishell.h"
 
-int		ft_cd(char *path)
+int		ft_cd(char **path, char *home)
 {
-	if (chdir(path) == 0)
+	int i;
+
+	i = 0;
+	while (path[i])
+		i++;
+	if (i == 2 && chdir(path[1]) == 0)
 		return (1);
+	else if (i == 1)
+	{
+		chdir(home);
+		return (1);
+	}
+	else if (i > 2)
+		return (-2);
 	else
 		return (-1);
 }
--- srcs_minishell/get_command.c
+++ srcs_minishell/get_command.c
@@ -1,5 +1,45 @@
 #include "../header/minishell.h"
 
+char	*ft_strncpy(char *src, char *dest, int len)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0' && len > 0)
+	{
+		dest[i] = src[i];
+		i++;
+		len--;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_home_path(char **argenv)
+{
+	char	*path;
+	int 	i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (argenv[i])
+	{
+		if (ft_strncmp(argenv[i], "HOME=", 5) == 0)
+		{
+			while (argenv[i][j] != '=')
+				j++;
+			j++;
+			path = malloc(sizeof(char) * ft_strlen(argenv[i]) - j);
+			ft_strncpy(&argenv[i][j], path, ft_strlen(argenv[i]) - j);
+			break;
+		}
+		i++;
+	}
+	return (path);
+}
+
+
 void	ft_get_command(char **args, char **argenv)
 {
 	char 	*commands[7] = {
@@ -22,10 +62,12 @@ void	ft_get_command(char **args, char **argenv)
 		&ft_exit
 	};
 
-	int i;
-	int j;
-	int	found; //1 = success, 0 = command not found, -1 = wrong path/file
+	int 	i;
+	int 	j;
+	int		found; //1 = success, 0 = command not found, -1 = wrong path/file, -2 = trop d'args
+	char	*home_path;
 
+	home_path = ft_home_path(argenv);
 	i = 0;
 	j = 0;
 	found = 0;
@@ -37,7 +79,7 @@ void	ft_get_command(char **args, char **argenv)
 			if (ft_strcmp(args[i], commands[j]) == 0 && ft_nopipes(args))
 				{
 					if (j == 1)
-						found = function[j](args[1]);
+						found = function[j](args, home_path);
 					else
 						found = function[j](argenv);
 				}
@@ -51,6 +93,10 @@ void	ft_get_command(char **args, char **argenv)
 	else if (found == -1)
 	{
 		ft_putstr(args[1]);
-		ft_putstr(" : No file or folder of this type");
+		ft_putstr(" : No file or folder of this type ");
+	}
+	else if (found == -2)
+	{
+		ft_putstr(" Too many arguments ");
 	}
 }
