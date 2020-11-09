cc122c77b90c6ec94f66033acfb62efa7653ef99
Diane <64610122+DianeHemi@users.noreply.github.com>
2020-11-09 21:23:36 +0100

    Merge problems corrected

--- srcs_minishell/ft_echo.c
+++ srcs_minishell/ft_echo.c
@@ -52,11 +52,7 @@ void	ft_single_qt(t_echo *config, char *args)
 
 
 
-<<<<<<< HEAD
 void	ft_double_qt(t_echo *config, char *args, char **argenv, int signal)
-=======
-void	ft_double_qt(t_echo *config, char *args, char **argenv)
->>>>>>> 65d886d1e94c08551b59d230a3bb8b0cb19f106e
 {
 	int 	i;
 
@@ -68,7 +64,6 @@ void	ft_double_qt(t_echo *config, char *args, char **argenv)
 		if (args[i] == '\"')
 			config->db_qt--;
 		if (config->db_qt == 0 && args[i + 1] == '\'')
-<<<<<<< HEAD
 		{
 			ft_single_qt(config, &args[i + 1]);
 			return ;
@@ -78,17 +73,6 @@ void	ft_double_qt(t_echo *config, char *args, char **argenv)
 			ft_putnbr_fd(signal, 1); //Doit renvoyer signal emit par derniere commande
 			i += 2;			 //0=reussite, -1 = echec. Et on avance jusqu'à après le '?'
 		}
-=======
-		{
-			ft_single_qt(config, &args[i + 1]);
-			return ;
-		}
-		/*else if (args[i][j] == '$' && args[i][j + 1] == '?')
-		{
-			ft_get_signal(); //Doit renvoyer signal emit par derniere commande
-			j += 2;			 //0=reussite, -1 = echec. Et on avance jusqu'à après le '?'
-		}*/
->>>>>>> 65d886d1e94c08551b59d230a3bb8b0cb19f106e
 		if (args[i] == '$' && !ft_strchr(" \"\\\'\0\n", args[i + 1]))
 		{
 			config->var_name = ft_extract_var_name(args, &i);
@@ -130,39 +114,19 @@ int		ft_echo(t_shell *shell)
 			ft_putstr("Wrong number of quotes :");
 			return (-2);
 		}
-<<<<<<< HEAD
 		if (config.token == 1)  //Gestion avec ''
 			ft_single_qt(&config, shell->args[i]);
 		else if (config.token == 2)  //Gestion avec ""
 			ft_double_qt(&config, shell->args[i], shell->argenv, shell->signal);
 		else if (config.token == 0)  //Gestion sans quotes
-=======
-
-		//Gestion avec ''
-		if (config.token == 1)
-			ft_single_qt(&config, shell->args[i]);
-		//Gestion avec ""
-		else if (config.token == 2)
-			ft_double_qt(&config, shell->args[i], shell->argenv);
-		//Gestion sans quotes
-		else if (config.token == 0)
->>>>>>> 65d886d1e94c08551b59d230a3bb8b0cb19f106e
 		{
 		//A gerer : else if (args[i][j] == '~')
 					//ft_putstr(getcwd(str, 256));
 		}
 		else
-<<<<<<< HEAD
 			return (shell->signal = -1); //Pas de token = erreur
 		ft_putstr(" ");
 		i++;
 	}
 	return (shell->signal = 1);
-=======
-			return (-1); //Pas de token = erreur
-		ft_putstr(" ");
-		i++;
-	}
-	return (1);
->>>>>>> 65d886d1e94c08551b59d230a3bb8b0cb19f106e
 }
---
+++ srcs_minishell/ft_parser.c
@@ -0,0 +1,176 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_parser.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lnoaille <lnoaille@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/06 16:48:44 by lnoaille          #+#    #+#             */
+/*   Updated: 2020/11/09 18:36:42 by lnoaille         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../header/minishell.h"
+
+
+void ft_strndup(char **dest, char *src, size_t start, size_t end)
+{
+	char *str;
+	size_t i;
+	i = 0;
+
+
+	str = malloc(sizeof(char) * (end - start + 1));
+	while (start < end && src[start])
+	{
+		str[i] = src[start];
+		start++;
+		i++;
+	}
+	str[i] = '\0';
+	*dest = str;
+}
+
+int not_a_sep(char letter)
+{
+	if (letter == ';' || letter == '>' || letter == '<' || letter == '|')
+		return (0);
+	return (1);
+}
+
+
+void iterate_word(char *str, size_t *i, size_t p)
+{
+	char sep = str[*i];
+	while((str[*i] == '"' || str[*i] == '\'') && str[*i])
+	{
+		if (str[*i] == sep)
+			p++;
+		(*i)++;
+	}
+	while((str[*i] != '"' && str[*i] != '\'' && str[*i]))
+	{
+		(*i)++;
+	}
+	while((str[*i] == '"' || str[*i] == '\'') && str[*i])
+	{
+		if (str[*i] == sep)
+			p++;
+		(*i)++;
+	}
+	if (((str[*i] != ' ' && not_a_sep(str[*i])) || (p%2 != 0)) && str[*i])
+		iterate_word(str, i, p);
+}
+
+size_t count_tokens(char *str)
+{
+	char **tab;
+	size_t i = 0;
+	size_t j = 0;
+	size_t p = 0;
+	size_t d = 0;
+
+	while (str[i])
+	{
+		while(str[i] == ' ')
+			i++;
+		j = i;
+		if (str[i] == '\'' || str[i] == '"')
+		{
+			iterate_word(str, &i, 0);
+			d = i;
+		}
+		while (not_a_sep(str[i]) && str[i] && str[i] != ' ')
+		{
+			i++;
+		}
+		d = i;
+		if (j != d)
+		{
+			p++;
+		}
+		if (str[i] == '>' && str[i+1] == '>')
+		{
+			i += 1;
+			p++;
+		}
+		else if (!not_a_sep(str[i]))
+		{
+					p++;
+		}
+		if (str[i])
+			i++;
+	}
+	return (p);
+}
+
+char **ft_parser(char *str)
+{
+	char **tab;
+	size_t i = 0;
+	size_t j = 0;
+	size_t p = 0;
+	size_t d = 0;
+
+	tab = malloc(sizeof(char *) * (count_tokens(str) + 1));
+	while (str[i])
+	{
+		while(str[i] == ' ')
+			i++;
+		j = i;
+		if (str[i] == '\'' || str[i] == '"')
+		{
+			iterate_word(str, &i, 0);
+			d = i;
+		}
+		while (not_a_sep(str[i]) && str[i] && str[i] != ' ')
+		{
+			i++;
+		}
+		d = i;
+		if (j != d)
+		{
+			ft_strndup(&tab[p], str, j , i );
+			p++;
+		}
+		if (str[i] == '>' && str[i+1] == '>')
+		{
+			ft_strndup(&tab[p], str, i , i+2);
+			i += 1;
+			p++;
+		}
+		else if (!not_a_sep(str[i]))
+		{
+			ft_strndup(&tab[p], str, i , i+1);
+					p++;
+		}
+		if (str[i])
+			i++;
+	}
+	tab[p] = NULL;
+	return (tab);
+}
+
+void ft_free_tab_made_by_parser(char **tab)
+{
+	size_t i = 0;
+	while (tab[i])
+	{
+		free(tab[i]);
+		i++;
+	}
+	free(tab);
+}
+
+// int main()
+// {
+// 	char **tab;
+// 	size_t i = 0;
+// 	tab = ft_parser("'test' 'test'");
+// 	while (tab[i])
+// 	{
+// 		printf("%s_\n", tab[i]);
+// 		i++;
+// 	}
+// 	ft_free_tab_made_by_parser(tab);
+// }
