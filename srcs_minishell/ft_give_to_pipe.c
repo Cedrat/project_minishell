5fda3c27fb00467ec6a2040b16ec797af619e106
Cedrat <loicnoai@gmail.com>
2020-12-07 20:41:53 +0100

    :recycle: 42 NORME

--- srcs_minishell/ft_give_to_pipe.c
+++ srcs_minishell/ft_give_to_pipe.c
@@ -6,68 +6,83 @@
 /*   By: lnoaille <lnoaille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 16:35:36 by lnoaille          #+#    #+#             */
-/*   Updated: 2020/12/05 19:12:50 by lnoaille         ###   ########.fr       */
+/*   Updated: 2020/12/07 20:31:28 by lnoaille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/minishell.h"
 
-int ft_give_to_pipe(t_shell *shell)
+static void		new_argument(t_shell *shell, char *arg_pipe)
 {
-		int fd[512][2];
-		int i = 0;
-		char **args_pipes;
-		int tmpin;
-		int tmpout;
+	ft_free_tab(shell->args);
+	shell->args = ft_parser(arg_pipe, shell);
+	ft_choose_fd(shell);
+}
 
-		tmpin = dup(1);
-		tmpout = dup(2);
-		args_pipes = ft_divide_to_pipe(shell->args);
+static void		cleans(int fdin, int fdout, t_shell *shell)
+{
+	if (dup2(fdin, 0) == -1)
+		exit(0);
+	if (dup2(fdout, 0) == -1)
+		exit(0);
+	free_shell_commands(shell);
+	free(shell->echo);
+	ft_free_tab(shell->args);
+}
 
+static void		son_works(int fd[512][2], t_shell *shell,
+													char **args_pipes, size_t i)
+{
+	int pid;
 
-		while (args_pipes[i + 1])
+	if (pipe(fd[i]) == -1)
+		exit(0);
+	if ((pid = fork()) == 0)
+	{
+		close(fd[i][0]);
+		dup2(fd[i][1], 1);
+		close(fd[i][1]);
+		if (i > 0)
 		{
-			ft_free_tab(shell->args);
-			shell->args = ft_parser(args_pipes[i], shell);
-			ft_choose_fd(shell);
-			pipe(fd[i]);
-			if (fork() == 0)
-			{
-				close(fd[i][0]);
-				dup2(fd[i][1], 1);
-				close(fd[i][1]);
-
-				if (i > 0)
-				{
-					dup2(fd[i - 1][0], 0);
-					close(fd[i - 1][0]);
-				}
-				ft_get_command(shell);
-				ft_free_tab(args_pipes);
-				ft_free_all(shell);
-				exit(0);
-			}
-			else
-			{
-				close(fd[i][1]);
-				if (i > 0)
-				{
-					close(fd[i - 1][0]);
-				}
-				i++;
-			}
+			dup2(fd[i - 1][0], 0);
+			close(fd[i - 1][0]);
 		}
-		dup2(fd[i - 1][0], 0);
-		close(fd[i - 1][0]);
-		ft_free_tab(shell->args);
-		shell->args = ft_parser(args_pipes[i], shell);
-		ft_choose_fd(shell);
 		ft_free_tab(args_pipes);
 		ft_get_command(shell);
-		dup2(tmpin, 0);
-		dup2(tmpout, 1);
-		wait(NULL);
-		free_shell_commands(shell);
-		free(shell->echo);
-		ft_free_tab(shell->args);
+		ft_free_all(shell);
+		exit(0);
+	}
+	else
+	{
+		close(fd[i][1]);
+		if (i > 0)
+			close(fd[i - 1][0]);
+	}
+}
+
+int				ft_give_to_pipe(t_shell *shell)
+{
+	int		fd[512][2];
+	size_t	i;
+	char	**args_pipes;
+	int		tmpin;
+	int		tmpout;
+
+	i = 0;
+	tmpin = dup(1);
+	tmpout = dup(2);
+	args_pipes = ft_divide_to_pipe(shell->args);
+	while (args_pipes[i + 1])
+	{
+		new_argument(shell, args_pipes[i]);
+		son_works(fd, shell, args_pipes, i);
+		i++;
+	}
+	dup2(fd[i - 1][0], 0);
+	close(fd[i - 1][0]);
+	new_argument(shell, args_pipes[i]);
+	ft_free_tab(args_pipes);
+	ft_get_command(shell);
+	cleans(tmpin, tmpout, shell);
+	wait(NULL);
 }
