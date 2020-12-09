2eeadfd74e38145481a4ed4b9401aa034c8fca59
Cedrat <loicnoai@gmail.com>
2020-12-09 20:56:28 +0100

    :refactor: move syntax for fd in syntax error.

--- srcs_minishell/ft_choose_fd.c
+++ srcs_minishell/ft_choose_fd.c
@@ -6,7 +6,7 @@
 /*   By: lnoaille <lnoaille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 16:51:28 by lnoaille          #+#    #+#             */
-/*   Updated: 2020/12/08 18:11:28 by lnoaille         ###   ########.fr       */
+/*   Updated: 2020/12/09 20:07:24 by lnoaille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,14 +27,9 @@ int ft_choose_fd(t_shell *shell)
 				ft_errors(-4, shell);
 				return(0);
 			}
-			if (!ft_strcmp(shell->args[i + 1], ">") || !ft_strcmp(shell->args[i + 1], ">>") || !ft_strcmp(shell->args[i + 1], "<"))
-			{
-				ft_strcat("minishell: syntax error near the unexpected symbol «", shell->args[i], "»\n");
-				return (0); // gerer signal error
-			}
 			if (shell->fd != 1)
 				close(shell->fd);
-			shell->fd = open(shell->args[i + 1], O_WRONLY|O_CREAT|O_TRUNC|O_APPEND|O_NONBLOCK, 0644);
+			shell->fd = open(shell->args[i + 1], O_WRONLY|O_CREAT|O_TRUNC|O_NONBLOCK, 0644);
 			shell->args = ft_remove_in_tab(shell->args, shell->args[i + 1]);
 			shell->args = ft_remove_in_tab(shell->args, shell->args[i]);
 		}
@@ -44,11 +39,6 @@ int ft_choose_fd(t_shell *shell)
 			{
 				ft_errors(-4, shell);
 				return(0);
-			}
-			if (!ft_strcmp(shell->args[i + 1], ">") || !ft_strcmp(shell->args[i + 1], ">>") || !ft_strcmp(shell->args[i + 1], "<"))
-			{
-				ft_strcat("minishell: syntax error near the unexpected symbol «", shell->args[i], "»\n");
-				return (0); // gerer signal error
 			}
 			if (shell->fd != 1)
 				close(shell->fd);
@@ -62,11 +52,6 @@ int ft_choose_fd(t_shell *shell)
 			{
 				ft_errors(-4, shell);
 				return(0);
-			}
-			if (!ft_strcmp(shell->args[i + 1], ">") || !ft_strcmp(shell->args[i + 1], ">>") || !ft_strcmp(shell->args[i + 1], "<"))
-			{
-				ft_strcat("minishell: syntax error near the unexpected symbol «", shell->args[i], "»\n");
-				return (0) ; // gerer signal error
 			}
 			shell->args = ft_remove_in_tab(shell->args, shell->args[i]);
 			if (ft_str_is_present(shell->args, "<"))
--- srcs_minishell/syntax.c
+++ srcs_minishell/syntax.c
@@ -6,7 +6,7 @@
 /*   By: lnoaille <lnoaille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/09 15:52:36 by lnoaille          #+#    #+#             */
-/*   Updated: 2020/12/09 16:44:57 by lnoaille         ###   ########.fr       */
+/*   Updated: 2020/12/09 20:46:47 by lnoaille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,19 @@ int is_right_syntax(char **line)
 			ft_strcat("minishell: syntax error near the unexpected symbol «", line[i], "»\n");
 			ft_free_tab(line);
 			return (0);
+		}
+		else if ((!ft_strcmp(line[i], ">") || !ft_strcmp(line[i], ">>") || !ft_strcmp(line[i], "<")) && line[i+1] &&
+			(!ft_strcmp(line[i + 1], "<") || !ft_strcmp(line[i + 1], ">") || !ft_strcmp(line[i + 1], ">>" ) || !ft_strcmp(line[i + 1], "|")))
+			{
+				ft_strcat("minishell: syntax error near the unexpected symbol « ", line[i + 1], " »\n");
+				ft_free_tab(line);
+				return (0); //
+			}
+		else if ((!ft_strcmp(line[i], ">") || !ft_strcmp(line[i], ">>") || !ft_strcmp(line[i], "<")) && line[i + 1] == NULL)
+		{
+			ft_strcat("minishell: syntax error near the unexpected symbol «", " newline ", "»\n");
+			ft_free_tab(line);
+			return (0); // gerer signal error
 		}
 		i++;
 	}
