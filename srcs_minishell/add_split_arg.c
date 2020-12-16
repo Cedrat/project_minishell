c8b5449b835975b2172ef4abbb1996ab983b88ac
Cedrat <loicnoai@gmail.com>
2020-12-16 18:46:42 +0100

    :adhesive_bandage: FIX unintianilised variable

--- srcs_minishell/add_split_arg.c
+++ srcs_minishell/add_split_arg.c
@@ -6,11 +6,17 @@
 /*   By: lnoaille <lnoaille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/10 03:06:45 by lnoaille          #+#    #+#             */
-/*   Updated: 2020/12/16 18:27:01 by lnoaille         ###   ########.fr       */
+/*   Updated: 2020/12/16 18:44:45 by lnoaille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/minishell.h"
+
+static	void	copy_tab(char **new_tab, char **tab, size_t p, size_t l)
+{
+	if ((new_tab[p + l] = ft_strdup(tab[p])) == NULL)
+		exit(0);
+}
 
 static	void	init_tab_with_two_tabs(char **new_tab, char **str,
 												char **tab, int i)
@@ -46,16 +52,15 @@ char			**add_split_arg(char **tab, int *i)
 
 	p = 0;
 	l = 0;
+	str = NULL;
+	new_tab = NULL;
 	init_tab_with_two_tabs(new_tab, str, tab, *i);
 	while (tab[p])
 	{
 		if ((int)p == *i)
 			insert_tab(&l, p, new_tab, str);
 		else
-		{
-			if ((new_tab[p + l] = ft_strdup(tab[p])) == NULL)
-				exit(0);
-		}
+			copy_tab(new_tab, tab, p, l);
 		p++;
 	}
 	ft_free_tab(tab);
