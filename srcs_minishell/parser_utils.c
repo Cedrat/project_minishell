308b30fe879aebf2e56752fda19836cacb42748a
Cedrat <loicnoai@gmail.com>
2020-12-18 15:10:23 +0100

    :adhesive_bandage: FIX : var_env with quotes works

--- srcs_minishell/parser_utils.c
+++ srcs_minishell/parser_utils.c
@@ -6,11 +6,20 @@
 /*   By: lnoaille <lnoaille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/17 17:01:14 by lnoaille          #+#    #+#             */
-/*   Updated: 2020/12/17 17:55:15 by lnoaille         ###   ########.fr       */
+/*   Updated: 2020/12/18 15:08:34 by lnoaille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/minishell.h"
+
+static	void	check_signal(char **tab, t_shell *shell, int i)
+{
+	if (ft_strcmp(tab[i], "$?") == 0)
+	{
+		free(tab[i]);
+		tab[i] = ft_itoa(shell->signal);
+	}
+}
 
 char			*ft_replace_var(t_shell *shell, char *arg)
 {
@@ -62,24 +71,24 @@ static	int		ft_check_doll(char *tab)
 
 char			**ft_dollar(char **tab, t_shell *shell)
 {
-	int i;
-	int j;
+	int		i;
+	int		j;
+	char	*str;
 
 	i = 0;
 	j = 0;
 	while (tab[i])
 	{
+		str = ft_str_treatement(tab[i]);
+		free(tab[i]);
+		tab[i] = str;
 		if ((i > 0 && ft_strcmp(tab[i - 1], "echo") == 0)
 			|| ((tab[i][0] == '\'')))
 			break ;
-		if (ft_strcmp(tab[i], "$?") == 0)
-		{
-			free(tab[i]);
-			tab[i] = ft_itoa(shell->signal);
-		}
 		if ((j = ft_check_doll(tab[i])) > 0)
 			tab[i] = ft_cut_replace(tab[i], shell, j);
-		else if (tab[i][j] == '$' && tab[i][j + 1] && tab[i][j + 1] != '?')
+		check_signal(tab, shell, i);
+		if (tab[i][j] == '$' && tab[i][j + 1] && tab[i][j + 1] != '?')
 		{
 			tab[i] = ft_replace_var(shell, tab[i]);
 			tab = add_split_arg(tab, &i);
